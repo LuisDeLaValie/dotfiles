0d4ff37bfa8eac1c26f66fb04b461a1497d5ae18
Emilio <tdtxle@gmail.com>
2020-08-21 13:12:52 -0500

    iniciar

---
+++ config/bspwm/scripts/open_proyects.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env dash
+
+case "$1" in
+	1) code --folder-uri /home/bspwnemilio/Documentos/proyectos/flutter/guia2020/ && code --folder-uri /home/bspwnemilio/Documentos/proyectos/flutter/driver_taxiApp/ && code --folder-uri /home/bspwnemilio/Documentos/proyectos/flutter/taxiApp/;;
+    2) code --folder-uri /home/bspwnemilio/.config/sxhkd/ /home/bspwnemilio/.config/compton/ /home/bspwnemilio/.xinitrc /home/bspwnemilio/.config/bspwm/ /home/bspwnemilio/.config/polybar/;;
+    3) code ;;
+    4) code ;;
+    5) code ;;
+    6) code ;;
+    7) code ;;
+    8) code ;;
+    9) code ;;
+
+esac
